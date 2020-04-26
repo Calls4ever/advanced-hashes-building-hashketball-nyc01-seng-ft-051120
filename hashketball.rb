805d374c743c3e0c76ca98e1ed86f9ea580571dc
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-26 17:26:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -28,8 +28,8 @@ players:{
 }
 },
   away: {
-    team_name: "Charlotte Hornets"
-    colors: ["Turquoise", "Purple"]
+    team_name: "Charlotte Hornets",
+    colors: ["Turquoise", "Purple"],
     players:{
       jeff_adrien:{
         name: "Jeff Adrien", number:4 ,shoe:18 ,points:10 ,rebounds:1 ,assists:1 ,steals:2 ,blocks:7 , slam_dunks:2
